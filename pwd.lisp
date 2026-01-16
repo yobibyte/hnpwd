243608a04582e77f97de47a3ac959436fad828dd
Susam Pal <susam@susam.net>
2026-01-16 03:50:06 +0000

    Sort entries correctly

--- pwd.lisp
+++ pwd.lisp
@@ -135,6 +135,12 @@
   :hnuid "ciju"
   :bio "Building mental models around programming, finance and other topics.")
 
+ (:name "Cookie Engineer"
+  :site "https://cookie.engineer/"
+  :blog "https://cookie.engineer/weblog/"
+  :feed "https://cookie.engineer/weblog/feed.xml"
+  :hnuid "cookiengineer"
+  :bio "Writing and programming about cyber security, red teaming and purple teaming.")
 
  (:name "Damian Walsh"
   :site "https://damianwalsh.co.uk/"
@@ -142,13 +148,6 @@
   :feed "https://damianwalsh.co.uk/feed.xml"
   :hnuid "damianwalsh"
   :bio "I'm Damian, a designer based in Manchester, UK.")
-
- (:name "Cookie Engineer"
-  :site "https://cookie.engineer/"
-  :blog "https://cookie.engineer/weblog/"
-  :feed "https://cookie.engineer/weblog/feed.xml"
-  :hnuid "cookiengineer"
-  :bio "Writing and programming about cyber security, red teaming and purple teaming.")
 
  (:name "Dennis Silin"
   :site "https://marscalendar.space/"
